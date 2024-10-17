318e82a3cb7a5b8c586fe9356a617e7124e14ecb
alexpvtaa <alejandro.lopez02@estudiante.uam.es>
2024-10-17 20:03:18 +0200

    .

--- p1/p1_tarea4.py
+++ p1/p1_tarea4.py
@@ -92,7 +92,7 @@ def run_fusion(imgA, imgB, mask, niveles):
     return Gpyr_imgA, Gpyr_imgB, Gpyr_mask, Lpyr_imgA, Lpyr_imgB, Lpyr_fus, Lpyr_fus_rec
 if __name__ == "__main__":    
     
-    path_imagenes = "C:/Users/2alex/PycharmProjects/TSV/p1/img/"
+    path_imagenes = "./p1/img/"
 
     print("Practica 1 - Tarea 4 - Test autoevaluación\n")    
     result,imgAgray,imgBgray,maskgray,\
